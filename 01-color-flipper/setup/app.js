123474e9c4adc88f2fe8cdbbaf2a0f37ee1723a0
olzed <111131185+olzed@users.noreply.github.com>
2024-05-15 20:14:01 +0100

    colour flipper completed

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,9 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const colour = document.querySelector(".color")
+
+btn.addEventListener("click", function() {
+    const randNum = Math.floor(Math.random() * 4);
+    document.body.style.backgroundColor = colors[randNum];
+    colour.textContent = colors[randNum];
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn")
+const color = document.getElementById("color")
+
+btn.addEventListener("click", function() {
+    let hexColor = '#'
+    const randNum = Math.floor(Math.random() * 3) + 3
+    for (let i = 0; i < randNum; i++) {
+        hexColor += hex[getRandNum()]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function getRandNum() {
+    return Math.floor(Math.random() * hex.length);
+}
